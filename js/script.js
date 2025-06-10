858eb5d88f9414865731c86ead153f805fe51998
DanielRd27 <daniel.deodato@aluno.senai.br>
2025-06-10 16:31:16 -0300

    começo sprint 2
    desenvolvimento da main page e escopo do DB

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ let buttonLoginToRegister = document.getElementById("btn-redirection-register");
 // Main card
 let mainCard = document.getElementById("mainCard");
 
+let box = document.querySelector(".hover");
+let boxContainer = document.querySelector(".itemSlider");
+
 buttonLoginToRegister.onclick = () => {
     mainCard.classList.add("registerActive")
     mainCard.classList.remove("loginActive")
@@ -16,3 +19,13 @@ buttonRegisterToLogin.onclick = () => {
     mainCard.classList.remove("registerActive")
     mainCard.classList.add("loginActive")
 };
+
+boxContainer.addEventListener('mouseover', () => {
+  box.style.transition = 'transform 1s';
+  box.style.opacity = 'opacity 1';
+});
+
+boxContainer.addEventListener('mouseout', () => {
+  box.style.transition = 'transform 0s';
+  box.style.opacity = 'opacity 0';
+});
